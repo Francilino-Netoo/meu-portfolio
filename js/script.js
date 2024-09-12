c132c75b96050a3fbd242d20d15dd42bbe8837dc
Francilino-Netoo <francilino12345@gmail.com>
2024-09-12 18:40:00 -0300

    fefafaf

--- js/script.js
+++ js/script.js
@@ -2,6 +2,11 @@
 let menuIcon = document.querySelector("#menu-icon");
 let navbar = document.querySelector(".navbar");
 
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle("bx-x");
+  navbar.classList.toggle("active");
+};
+
 /*==================== scroll sections active link ====================*/
 let sections = document.querySelectorAll("section");
 let navLinks = document.querySelectorAll("header nav a");
@@ -13,11 +18,50 @@ window.onscroll = () => {
     let height = sec.clientHeight;
     let id = sec.getAttribute("id");
 
-    if (top >= offset) {
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach((links) => {
+        links.classList.remove("active");
+        document
+          .querySelector("header nav a[href*=" + id + "]")
+          .classList.add("active");
+      });
     }
   });
+
+  let header = document.querySelector("header");
+  header.classList.toggle("sticky", window.scrollY > 100);
+
+  menuIcon.classList.remove("bx-x");
+  navbar.classList.remove("active");
 };
 
 /*==================== scroll reveal ====================*/
+ScrollReveal({
+  //reset: true,
+  distance: "80px",
+  duratio: 5000,
+  delay: 500,
+});
 
+ScrollReveal().reveal(".home-contente, .heading", { origin: "top" });
+ScrollReveal().reveal(
+  ".home-img, .services-container, .portfolio-box, .contact form",
+  { origin: "bottom" }
+);
+ScrollReveal().reveal(".home-contente h1, .about-img", { origin: "left" });
+ScrollReveal().reveal(".home-contente p, .about-content", { origin: "right" });
 /*==================== typed js ====================*/
+
+const typed = new Typed(".multiple-text", {
+  strings: [
+    "Desenvolvedor Full Stack",
+    "Especialista em Scraping",
+    "Desenvolvedor Mobile",
+    "Desenvolvedor Mobile",
+    "Especialista em Automação Web",
+  ],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDalay: 1000,
+  loop: true,
+});
